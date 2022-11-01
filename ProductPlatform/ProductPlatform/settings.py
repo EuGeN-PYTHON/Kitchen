e50c663946b3736fbb02da6a9428b1d15de8232b
EuGeN-PYTHON <78923547+EuGeN-PYTHON@users.noreply.github.com>
2022-11-01 13:31:33 +0300

    update settings.py

--- ProductPlatform/ProductPlatform/settings.py
+++ ProductPlatform/ProductPlatform/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'orders',
-    'users',
+    'ProductPlatform.orders',
+    'ProductPlatform.users',
     'django_filters',
     # 'corsheaders',
 ]
